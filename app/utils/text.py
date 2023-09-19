efd82016e74cd4fe2c39eba05356c1fa9648aa53
NyaightHazard <13866033+NyaightHazard@users.noreply.github.com>
2023-09-20 03:15:10 +0900

    FIX use ASCII flag for non-ASCII character in article title

--- app/utils/text.py
+++ app/utils/text.py
@@ -3,6 +3,7 @@ import unicodedata
 
 
 def slugify(text: str) -> str:
+    #!FIX use ASCII flag for non-ASCII character in article title
     value = unicodedata.normalize("NFKC", text)
-    value = re.sub(r"[^\w\s-]", "", value.lower())
-    return re.sub(r"[-\s]+", "-", value).strip("-_")
+    value = re.sub(r"(?a)[^\w\s-]", "", value.lower())
+    return re.sub(r"(?a)[-\s]+", "-", value).strip("-_") or "post"
