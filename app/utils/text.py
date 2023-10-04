b56c14eab5a245b4706240d9657e783ea4f67436
NyaightHazard <13866033+NyaightHazard@users.noreply.github.com>
2023-10-04 21:12:38 +0900

    more safety for empty article title

--- app/utils/text.py
+++ app/utils/text.py
@@ -1,9 +1,9 @@
 import re
 import unicodedata
-
+from secrets import token_hex
 
 def slugify(text: str) -> str:
     #!FIX use ASCII flag for non-ASCII character in article title
     value = unicodedata.normalize("NFKC", text)
     value = re.sub(r"(?a)[^\w\s-]", "", value.lower())
-    return re.sub(r"(?a)[-\s]+", "-", value).strip("-_") or "post"
+    return re.sub(r"(?a)[-\s]+", "-", value).strip("-_") or ("post-"+token_hex(8))
