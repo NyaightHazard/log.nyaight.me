235932c6c0af3bdacc4542805006b97b0f5f7171
NyaightHazard <13866033+NyaightHazard@users.noreply.github.com>
2023-11-22 19:47:07 +0900

    more strict article title

--- app/utils/text.py
+++ app/utils/text.py
@@ -3,7 +3,7 @@ import unicodedata
 from secrets import token_hex
 
 def slugify(text: str) -> str:
-    #!FIX use ASCII flag for non-ASCII character in article title
+    #!MOD use ASCII flag for non-ASCII character in article title
     value = unicodedata.normalize("NFKC", text)
-    value = re.sub(r"(?a)[^\w\s-]", "", value.lower())
-    return re.sub(r"(?a)[-\s]+", "-", value).strip("-_") or ("post-"+token_hex(8))
+    value = re.sub(r"(?a)\W+", "-", value.lower())
+    return value.strip("_-") or ("post-"+token_hex(8))
